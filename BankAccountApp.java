49bb6e064cb3d131cd63160af4310e6546dd3123
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-22 18:28:53 +0530

    Update BankAccountApp.java

--- BankAccountApp.java
+++ BankAccountApp.java
@@ -1,1 +1,17 @@
+public class BankAccountApp
+{
+  public static void main(String[]args)
+  {
+    Checking chkacc1 = new Checking("Tom","321321321",1500);
+    Savings savacc1 = new Savings("Kevin","456678909",1000);
+    savacc1.showInfo();
+    chkacc1.showInfo();
+  
+  
+  
+  }
 
+
+
+
+}

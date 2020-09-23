265b32f629e2d7a44dda7d50ccda8d726bb3fe30
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-23 17:16:21 +0530

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,1 +1,38 @@
+public class Savings extends Account
+{
+  int safetyDepositBoxID;
+  int safetyDepositBoxKey;
+  
+  
+  public Savings(String name,String sSN,double initDeposit)
+  {
+    super(name,sSN,initDeposit);
+    accountNumber = "1"+accountNumber;
+    setSafetyDepositBox();
+ 
+  }
+  private void setSafetyDepositBox()
+  
+  {
+    safetyDepositBoxID =  (int)(Math.random()*Math.pow(10,3));
 
+    safetyDepositBoxKey = (int)(Math.random()*Math.pow(10,4));
+  
+  
+  
+  }
+  public void showInfo()
+  {
+  super.showInfo();
+  System.out.println("Your savings Account Features"+\n SafetyDepositBoxID:"+safetyDepositBoxID+"\n safetyDepositBoxKey:"+safetyDepositBoxKey);
+  
+  
+  
+  
+  }
+
+
+
+
+
+}

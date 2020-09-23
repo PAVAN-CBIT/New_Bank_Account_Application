33b521d70a84bb2174e9a8b67100b871d001a2a0
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-09-23 16:57:23 +0530

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,1 +1,43 @@
+public class Checking extends Account
+{
 
+  int debitCardNumber;
+  int debitCardPin;
+  
+  
+  public Checking(String name,String sSN,double initDeposit)
+  {
+     super(name,sSN, initDeposit);
+     accountNumber = "2"+accountNumber;
+     setDebitCard();
+  
+  
+  
+  }
+  
+  
+  private void setDebitCard()
+  
+  {
+    debitCardNumber = (int)(math.random()*math.pow(10,12));
+    debitCardPin =    (int)(math.random()*math.pow(10,4));
+
+  
+  
+  
+  
+  }
+  public void showInfo()
+  {
+   super.showInfo();
+   System.out.println("Your checking Account Features"+"\n Debit Card Number:"+debitCardNumber+"\n DebitCard PIN"+debitCardPin);
+  
+  
+  
+  
+  }
+
+
+
+
+}
